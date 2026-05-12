0d96689ac6851423b0d983b9badb790e7cbcedd1
Lucciano <luciano.s.renda@gmail.com>
2026-05-12 02:23:22 -0300

    add swayosd and nvim nuke

---
+++ nvim/.config/nvim/lua/plugins/snacks-picker.lua
@@ -0,0 +1,17 @@
+return {
+    "folke/snacks.nvim",
+    opts = {
+        picker = {
+            enabled = true,
+        },
+        projects = {
+            -- Add the directories where your projects live
+            dirs = {
+                "~/projects",
+                "~/code",
+                "~/dev",
+                "~/.config",
+            },
+        },
+    },
+}
